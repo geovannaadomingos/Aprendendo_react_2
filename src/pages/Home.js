6a68952acecd953872fc66fe36f817584513edee
Geovanna Domingos <gmdomingosn@gmail.com>
2021-06-18 22:46:53 -0300

    adiciona base do componente SectionCard

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import SectionCards from "../components/SectionCards"
+function Home() {
+  return (
+    <body>
+      <SectionCards title ="Consumindo API do Harry Potter" />
+    </body>
+  );
+}
+
+export default Home;
